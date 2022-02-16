7436d540a4389bc4c957ff76431c9bc44b67ae7b
Marcos Henrique Pereira da Silva <marcos.silva@compasso.com.br>
2022-02-16 14:26:18 -0300

    IGOR DAS MENINAS

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ print("Test 1 2 3 ")
 remoteurl = "https://github.com/ok-marcos/galpaodamarinha.git"
 localfolder = "temp/galpaomarinha"
 
-myrepo = git.push(remoteurl, localfolder)
+myrepo = git.Repo.clone(remoteurl, localfolder)
 myrepo.git.checkout("master")
