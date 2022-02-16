d1188e702ff7f25e14c16a6ad431da22d6380f6a
Marcos Henrique Pereira da Silva <marcos.silva@compasso.com.br>
2022-02-16 10:51:04 -0300

    commit message from python script

--- testegit.py
+++ testegit.py
@@ -2,5 +2,5 @@ from git import Repo
 repo = Repo()
 repo.git.add('--all')
 repo.git.commit('-m', 'commit message from python script')
-origin = repo.remote(name='master')
+origin = repo.remote(name='orogin')
 origin.push()
